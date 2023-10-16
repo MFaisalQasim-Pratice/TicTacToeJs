5c791f0b6e1265f19b90b5e2db5c6c983c30555c
Muhammad FaisalQasim <faisalqasim876@gmail.com>
2023-10-16 09:31:25 -0700

    TicTacToe Done

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-const gameBoard = document.querySelector('#gameBoard');
-const msjBox = document.querySelector('#msjBox');
+const gameBoard = document.querySelector("#gameBoard");
+const msjBox = document.querySelector("#msjBox");
 
-const boardCells = [
-    "", "", "", "", "", "", "", "", ""
-]
+const boardCells = ["", "", "", "", "", "", "", "", ""];
+msjBox.textContent = "First is circle's turn";
+let turn = "circle";
 
-msjBox.textContent = "First is circle's turn"
-turn = 'circle'
-
-function createBoard() {    
-    boardCells.forEach((_square, element) => {
-        const boardSquare = document.createElement('div');
-        boardSquare.classList.add('boardSquare');
-        boardSquare.id = element;
-        addEventListener('click', onBoardClick);
-        gameBoard.append(boardSquare);
-    });
+function createBoard() {
+  boardCells.forEach((_, element) => {
+    const boardSquare = document.createElement("div");
+    boardSquare.classList.add("board-square");
+    boardSquare.id = element;
+    boardSquare.addEventListener("click", onBoardClick); // Use addEventListener here
+    gameBoard.append(boardSquare);
+  });
 }
-createBoard()
+createBoard();
 
 function onBoardClick(e) {
-    const mark = document.createElement('div');
-    mark.classList.add(turn);
-    e.target.append(mark);
-    turn = turn === "circle" ? "cross" : "circle";
-    msjBox.textContent =  `Now is ${turn}'s turn`
-    e.target.removeEventListener("click", onBoardClick);
+  const mark = document.createElement("div");
+  mark.classList.add(turn);
+  e.target.append(mark);
+  turn = turn === "circle" ? "cross" : "circle";
+  msjBox.textContent = `Now is ${turn}'s turn`;
+
+  // Remove the click event listener
+  e.target.removeEventListener("click", onBoardClick);
+  checkWinner();
+}
+
+function checkWinner() {
+    const allBoardSquare = document.querySelectorAll(".board-square");
+
+  winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winningCombos.forEach(combo => {
+    const circleWins = combo.every(cell => allBoardSquare[cell].firstChild?.classList.contains('circle'))
+    if (circleWins) {
+        msjBox.textContent = 'Circle Wins!';
+        allBoardSquare.forEach(square => square.replaceWith(square.cloneNode(true)))
+        return;
+    }
+    const crossWins = combo.every(cell => allBoardSquare[cell].firstChild?.classList.contains('cross'))
+    if (crossWins) {
+        msjBox.textContent = 'Cross Wins!';
+        allBoardSquare.forEach(square => square.replaceWith(square.cloneNode(true)))
+        return;
+    }
+  });
 }
